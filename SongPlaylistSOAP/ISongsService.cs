d0584d07104ccd86d035ffd489b11df8467fcf05
Tsvetozar Bonev <tsbonev@gmail.com>
2019-01-02 22:30:08 +0200

    Add SOAP service

---
+++ SongPlaylistSOAP/ISongsService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SongPlaylistSOAP
+{
+    [ServiceContract]
+    public interface ISongsService
+    {
+        [OperationContract]
+        List<SongsType> GetAllSongs();
+    }
+
+
+    [DataContract]
+    public class SongsType
+    {
+        public SongsType(string artist, string title, List<string> genres)
+        {
+            this.genres = genres;
+            this.artist = artist;
+            this.title = title;
+        }
+
+        List<string> genres;
+        string title;
+        string artist;
+
+        [DataMember]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        [DataMember]
+        public string Artist
+        {
+            get { return artist; }
+            set { artist = value; }
+        }
+
+        [DataMember]
+        public List<string> Genres
+        {
+            get { return genres; }
+            set { genres = value; }
+        }
+    }
+}
---
+++ SongPlaylistSOAP/SongService.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using SongPlaylistLib.Core;
+using SongPlaylistSOAP.Helpers;
+
+namespace SongPlaylistSOAP
+{
+    public class SongService : ISongsService
+    {
+
+        private IMusicPlaylist playlist = InMemoryPlaylistProvider.GetPlaylist();
+
+        public List<SongsType> GetAllSongs()
+        {
+
+            var songs = playlist.GetAll();
+
+            var songTypeList = new List<SongsType>();
+
+            songs.ForEach(it => songTypeList.Add(new SongsType(it.Title, it.Artist, it.Genres)));
+
+            return songTypeList;
+        }
+    }
+}
